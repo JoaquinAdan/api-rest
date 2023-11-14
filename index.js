dca739eb680e68b2536654847d9e9d582512d434
JoaquinAdan <rodriguezjdev@gmail.com>
2023-11-14 12:18:26 -0300

    Add express server with new endpoints

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('hola')
+// import express from 'express'
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('<div style="color: red;">Hello world!</div>')
+})
+
+app.get('/new-route', (req, res) => {
+  res.send('<div style="color: blue;">Am a new endpoint</div>')
+})
+
+app.get('/products', (req, res) => {
+  res.json({ name: 'iPhone X', price: 999 })
+})
+
+app.listen(port, () => {
+  console.log('Mi port ' + port)
+})
