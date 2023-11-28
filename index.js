57c3fc4cddcbedcc9cdfbc2f483bfee5e1fe20ab
JoaquinAdan <joacorodriguez.98l@gmail.com>
2023-11-28 20:12:48 -0300

    Refactor middleware and add error handling

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// import express from 'express'
 const express = require('express')
 const routerApi = require('./routes')
+const { logErrors, errorHandler } = require('./middlewares/error.handler')
 
 const app = express()
 const port = 3000
@@ -17,46 +17,9 @@ app.get('/new-route', (req, res) => {
 
 routerApi(app)
 
+app.use(logErrors)
+app.use(errorHandler)
+
 app.listen(port, () => {
   console.log('Mi port ' + port)
 })
-// app.get('/categories/:categoryId/products/:productId', (req, res) => {
-//   const { productId, categoryId } = req.params
-//   res.json({ categoryId, productId })
-// })
-
-// app.get('/:id', (req, res) => {
-//   const { id } = req.params
-//   res.json({
-//     id,
-//     name: 'iPhone X',
-//     price: 999,
-//   })
-// })
-
-// app.post('/product', (req, res) => {
-//   const body = req.body
-//   console.log(body)
-//   res.json({
-//     message: 'created',
-//     data: body,
-//   })
-// })
-
-// app.patch('/:id', (req, res) => {
-//   const { id } = req.params
-//   const body = req.body
-//   res.json({
-//     message: 'Update',
-//     data: body,
-//     id,
-//   })
-// })
-
-// app.delete('/:id', (req, res) => {
-//   const { id } = req.params
-//   res.json({
-//     message: 'Delete',
-//     id,
-//   })
-// })
