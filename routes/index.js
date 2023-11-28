59e2583925b81a4f36589a964b85e5878ead562d
JoaquinAdan <rodriguezjdev@gmail.com>
2023-11-28 14:46:05 -0300

    Refactor router initialization in index.js

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const productsRouter = require('./products.router')
 const usersRouter = require('./users.router')
 
-const router = express.Router()
 function routerApi(app) {
+  const router = express.Router()
   app.use('/api/v1', router)
   router.use('/products', productsRouter)
   router.use('/users', usersRouter)
